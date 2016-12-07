a035f5f0268d5025bcfb71e325019b588cb03454
Jeiti <jeiti@list.ru>
2016-12-07 13:34:45 +0300

    Создали тестовый проект

--- admin/config.php
+++ admin/config.php
@@ -1,25 +1,25 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://opencart.loc/admin/');
-define('HTTP_CATALOG', 'http://opencart.loc/');
+define('HTTP_SERVER', 'http://opencart_test.loc/admin/');
+define('HTTP_CATALOG', 'http://opencart_test.loc/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://opencart.loc/admin/');
-define('HTTPS_CATALOG', 'http://opencart.loc/');
+define('HTTPS_SERVER', 'http://opencart_test.loc/admin/');
+define('HTTPS_CATALOG', 'http://opencart_test.loc/');
 
 // DIR
-define('DIR_APPLICATION', '/home/evgen/www/opencart/admin/');
-define('DIR_SYSTEM', '/home/evgen/www/opencart/system/');
-define('DIR_IMAGE', '/home/evgen/www/opencart/image/');
-define('DIR_LANGUAGE', '/home/evgen/www/opencart/admin/language/');
-define('DIR_TEMPLATE', '/home/evgen/www/opencart/admin/view/template/');
-define('DIR_CONFIG', '/home/evgen/www/opencart/system/config/');
-define('DIR_CACHE', '/home/evgen/www/opencart/system/storage/cache/');
-define('DIR_DOWNLOAD', '/home/evgen/www/opencart/system/storage/download/');
-define('DIR_LOGS', '/home/evgen/www/opencart/system/storage/logs/');
-define('DIR_MODIFICATION', '/home/evgen/www/opencart/system/storage/modification/');
-define('DIR_UPLOAD', '/home/evgen/www/opencart/system/storage/upload/');
-define('DIR_CATALOG', '/home/evgen/www/opencart/catalog/');
+define('DIR_APPLICATION', '/home/evgen/www/opencart_test/admin/');
+define('DIR_SYSTEM', '/home/evgen/www/opencart_test/system/');
+define('DIR_IMAGE', '/home/evgen/www/opencart_test/image/');
+define('DIR_LANGUAGE', '/home/evgen/www/opencart_test/admin/language/');
+define('DIR_TEMPLATE', '/home/evgen/www/opencart_test/admin/view/template/');
+define('DIR_CONFIG', '/home/evgen/www/opencart_test/system/config/');
+define('DIR_CACHE', '/home/evgen/www/opencart_test/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/evgen/www/opencart_test/system/storage/download/');
+define('DIR_LOGS', '/home/evgen/www/opencart_test/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/evgen/www/opencart_test/system/storage/modification/');
+define('DIR_UPLOAD', '/home/evgen/www/opencart_test/system/storage/upload/');
+define('DIR_CATALOG', '/home/evgen/www/opencart_test/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
--- config.php
+++ config.php
@@ -1,22 +1,22 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://opencart.loc/');
+define('HTTP_SERVER', 'http://opencart_test.loc/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://opencart.loc/');
+define('HTTPS_SERVER', 'http://opencart_test.loc/');
 
 // DIR
-define('DIR_APPLICATION', '/home/evgen/www/opencart/catalog/');
-define('DIR_SYSTEM', '/home/evgen/www/opencart/system/');
-define('DIR_IMAGE', '/home/evgen/www/opencart/image/');
-define('DIR_LANGUAGE', '/home/evgen/www/opencart/catalog/language/');
-define('DIR_TEMPLATE', '/home/evgen/www/opencart/catalog/view/theme/');
-define('DIR_CONFIG', '/home/evgen/www/opencart/system/config/');
-define('DIR_CACHE', '/home/evgen/www/opencart/system/storage/cache/');
-define('DIR_DOWNLOAD', '/home/evgen/www/opencart/system/storage/download/');
-define('DIR_LOGS', '/home/evgen/www/opencart/system/storage/logs/');
-define('DIR_MODIFICATION', '/home/evgen/www/opencart/system/storage/modification/');
-define('DIR_UPLOAD', '/home/evgen/www/opencart/system/storage/upload/');
+define('DIR_APPLICATION', '/home/evgen/www/opencart_test/catalog/');
+define('DIR_SYSTEM', '/home/evgen/www/opencart_test/system/');
+define('DIR_IMAGE', '/home/evgen/www/opencart_test/image/');
+define('DIR_LANGUAGE', '/home/evgen/www/opencart_test/catalog/language/');
+define('DIR_TEMPLATE', '/home/evgen/www/opencart_test/catalog/view/theme/');
+define('DIR_CONFIG', '/home/evgen/www/opencart_test/system/config/');
+define('DIR_CACHE', '/home/evgen/www/opencart_test/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/evgen/www/opencart_test/system/storage/download/');
+define('DIR_LOGS', '/home/evgen/www/opencart_test/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/evgen/www/opencart_test/system/storage/modification/');
+define('DIR_UPLOAD', '/home/evgen/www/opencart_test/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
